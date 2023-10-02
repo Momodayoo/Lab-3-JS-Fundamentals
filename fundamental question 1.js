15074d318c6c065c2559292370bc807c446b5771
Momodayoo <143530573+Momodayoo@users.noreply.github.com>
2023-10-02 22:32:20 +0800

    commit on main

---
+++ fundamental question 1.js
@@ -0,0 +1,17 @@
+EQUATION            ANSWER
+"" + 1 + 0          // 10//
+"" - 1 + 0          // -1 //
+true + false        // 1 //
+!true               // false //
+6/ "3"              // 2 //
+"2" + "3"           // 23 //
+4 + 5 + "px"        // 9px //
+"$" + 4 + 5         // $45 //
+"4" - 2             // 2 //
+"4px" - 2           // Nan //
+"  -9  " + 5        // -9  + 5 //
+null + 1            // 1 //
+undefined + 1       // NaN // 
+undefined == null   // true // 
+undefined === null  // false //
+"  \t \n" - 2       // -2 //
---
+++ fundamental question 2.js
@@ -0,0 +1,30 @@
+const { DamerauLevenshteinDistance } = require("natural")
+
+let three = "3"
+let four = "4"
+let thirty = "30"
+
+// remove quotation mark on 3 and 4 so the math equation is correct 
+
+<!DOCTYPE html>
+<script> 
+
+let three = 3; 
+let four = 4;
+
+let thirty = "30";
+
+alert(three + four); // 3
+</script>
+
+
+// what is the value of the following expressions?
+QUESTIONS                                   ANSWER
+let addition = three + four                 4
+let multiplication = three * four           12    
+let division = three / four                 3/4 = 0.75
+let subtraction = three - four              -1
+
+let lessThan1 = three < four                true
+let lessThan2 = thirty < four               false
+
